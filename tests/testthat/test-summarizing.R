0a8f4112ee9a196b65248dda1394cd8d0612ce2f
tessaw02 <tessaw02@student.ubc.ca>
2024-11-08 12:48:45 -0800

    Updated test_that to contain my tests from assignment B1

--- tests/testthat/test-summarizing.R
+++ tests/testthat/test-summarizing.R
@@ -6,8 +6,16 @@ test_that("Only summarizes numerical data", {
 })
 
 test_that("data_summary output is a list", {
-  df <- data.frame(
+  df2 <- data.frame(
     value1 = c(1, 2, 3),
     value2 = c(4, 5, 6))
-  expect_type(data_summary(df), "list")
+  expect_type(data_summary(df2), "list")
+})
+
+test_that("data_summary contains correct statistics",{
+  df3 <- data.frame(
+    value1 = c(5, 15, 25),
+    value2 = c(7, 24, 32))
+  summarized <- data_summary(df3)
+  expect_equal(summarized$value1_mean, mean(df3$value1, na.rm = TRUE))
 })
